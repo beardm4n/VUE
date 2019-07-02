240fdf875ce7cd96584bcc517eb59fd56f0e818e
beardm4n <dmitriykorovko@gmail.com>
2019-07-02 09:04:29 +0300

    установил CLI, создал начальный проект

---
+++ test-project/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  // render: function (h) {
+  //   h(App)
+  // }
+}).$mount('#app')
